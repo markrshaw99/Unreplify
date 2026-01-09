8752cea66ebe3c9f5ed3cf4804273412327d7cd8
Mark <markrutherfordshaw@outlook.com>
2026-01-09 22:28:32 +0000

    Created the framework for my functions

---
+++ Unreplify.py
@@ -0,0 +1,21 @@
+import subprocess #Needed for accessing pbcopy and pbpaste
+
+def unreplify_main():
+    """
+    This Function will be the main function
+    1 - Access clipboard and store as variable
+    2 - Strip The Text of REPL prompts and output a clean formatted string
+    3 - Take this new variable and copy it to clipboard
+    """
+    return
+
+def retrieve_clipboard():
+    """Access using pbpaste"""
+    return()
+
+def set_clipboard():
+    """Set using pbcopy"""
+    return()
+
+def clean_string():
+    return()
